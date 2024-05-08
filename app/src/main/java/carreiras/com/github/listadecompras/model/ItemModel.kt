0b30b0ecd708a5a67368c55f12fb6037a6d0c724
Thouser12 <147556151+Thouser12@users.noreply.github.com>
2024-05-08 17:20:23 -0300

    Update ItemModel.kt

--- app/src/main/java/carreiras/com/github/listadecompras/model/ItemModel.kt
+++ app/src/main/java/carreiras/com/github/listadecompras/model/ItemModel.kt
@@ -1,6 +1,12 @@
-package carreiras.com.github.listadecompras.model
+package carreiras.com.github.listadecompras.model;
 
-data class ItemModel(
+public data class ItemModel(
     val name: String,
+
+    /**
+     * Ação de callback para remover o item da lista. É chamada quando
+     * o item precisa ser removido, permitindo a manipulação da ação de remoção
+     * diretamente no modelo de dados.
+     */
     val onRemove: (ItemModel) -> Unit
 )
